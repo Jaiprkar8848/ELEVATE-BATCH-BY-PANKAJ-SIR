4308b9610c55664aaecfdf742ae1654f504dcbe1
Jaiprkar8848 <jaiprkar8848@gmail.com>
2025-06-16 18:03:06 +0530

    add algo

--- Miscallenous/prac.c
+++ Miscallenous/prac.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 
+int reverse(int n){
+    int reversed=0;
+    while(n!=0){
+        reversed=reversed*10+n%10;
+        n/=10;
+    }
+    return reversed;
+}
+
 void main(){
-    int g=1/3*4/4-6/2+2/3*6/3;
-    printf("%d",g);
+    int n=12345;
+    n=reverse(n);
+    printf("%d",n);
 }
---
+++ Miscallenous/slasht.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+void main(){
+    printf("Jaiprak\tKarwa\n");
+    printf("Jaipraka\tKarwa\n");
+    printf("Jaiprakash\tKarwa");
+}

5e85a75f4ad34b16f7bf2cfb424f9a2d925e1c91
Jaiprkar8848 <jaiprkar8848@gmail.com>
2025-06-20 19:06:28 +0530

    20-6-2025

--- Miscallenous/prac.c
+++ Miscallenous/prac.c
@@ -1,20 +1,34 @@
 #include<stdio.h>
+#define size 10
+
+void insertAtEnd(int A[],int* n,int x){
+    A[*n]=x;
+    *n+=1;
+}
+
+void insertAtIdx(int A[],int* n,int idx,int x){
+    int i;
+    for(i=*n-1;i>idx;i--){
+        A[i+1]=A[i];
+    }
+    A[idx]=x;
+    *n+=1;
+}
+
+void print(int A[],int n){
+    int i;
+    for(i=0;i<n;i++){
+        printf("%d ",A[i]);
+    }
+    printf("\n");
+}
+
 void main(){
-    int n=5;
-    int i,j,spaces;
-    char ch='A';
-    for(i=1;i<=n;i++){
-        for(spaces=1;spaces<=n-i;spaces++)
-            printf(" ");
-        for(j=1;j<=2*i-1;j++)
-            printf("*");
-        printf("\n");
-    }
-    for(i=1;i<=n-1;i++){
-        for(spaces=1;spaces<=i;spaces++)
-            printf(" ");
-        for(j=1;j<=2*(n-i)-1;j++)
-            printf("*");
-        printf("\n");
-    }
+    int A[size]={10,20,30,40};
+    int n=4;
+    print(A,n);
+    insertAtEnd(A,&n,5);
+    print(A,n);
+    insertAtIdx(A,&n,2,4);
+    print(A,n);
 }

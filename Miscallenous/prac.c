dcd375d6e3faa5d9f643df8a7c411c0e8d368214
Jaiprkar8848 <jaiprkar8848@gmail.com>
2025-06-27 16:09:41 +0530

    27-6-2025

--- Miscallenous/prac.c
+++ Miscallenous/prac.c
@@ -1,34 +1,13 @@
 #include<stdio.h>
-#define size 10
 
-void insertAtEnd(int A[],int* n,int x){
-    A[*n]=x;
-    *n+=1;
+void f(int n){
+    static int d=1;
+    printf("%d",n);
+    printf("%d",d);
+    d=d+n;
+    if(n>1) f(--n);
+    printf("%d",d);
 }
-
-void insertAtIdx(int A[],int* n,int idx,int x){
-    int i;
-    for(i=*n-1;i>idx;i--){
-        A[i+1]=A[i];
-    }
-    A[idx]=x;
-    *n+=1;
-}
-
-void print(int A[],int n){
-    int i;
-    for(i=0;i<n;i++){
-        printf("%d ",A[i]);
-    }
-    printf("\n");
-}
-
-void main(){
-    int A[size]={10,20,30,40};
-    int n=4;
-    print(A,n);
-    insertAtEnd(A,&n,5);
-    print(A,n);
-    insertAtIdx(A,&n,2,4);
-    print(A,n);
+int main(){
+    f(3);
 }
